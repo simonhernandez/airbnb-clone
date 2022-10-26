104d7fbee2cab0cf39976827288c02ecf284de0b
Simon Hernandez Barboza <simonbarboza3008@gmail.com>
2022-10-25 23:10:15 -0400

    finish header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
+import { MagnifyingGlassIcon, GlobeAltIcon, MenuIcon, UserCircleIcon, UsersIcon, Bars3Icon } from '@heroicons/react/24/solid'
 
 const Header = () => {
   return (
@@ -16,13 +16,21 @@ const Header = () => {
         </div>
 
         {/* Center - Search Bar */}
-        <div className='flex items-center border-2 rounded-full py-2 px-1 md:shadow-sm'>
-          <input type="search" name="search" id="search" placeholder='Start your search' className='flex-1 mx-2 bg-transparent outline-none text-gray-600 text-sm' />
+        <div className='flex items-center md:border-2 rounded-full py-2 px-1 md:shadow-sm'>
+          <input type="search" name="search" id="search" placeholder='Start your search' className='flex-1 mx-2 bg-transparent outline-none text-gray-600 text-sm placeholder-gray-400' />
           <MagnifyingGlassIcon className='hidden md:inline-flex bg-red-400 h-8 text-white rounded-full p-2 cursor-pointer' />
         </div>
 
         {/* right */}
-        <div>r</div>
+        <div className='flex justify-end items-center space-x-4 text-gray-500'>
+          <p className='hidden md:inline cursor-pointer'>Become a host</p>
+          <GlobeAltIcon className='h-6 cursor-pointer' />
+
+          <div className='flex border-2 rounded-full p-2 space-x-2'>
+            <Bars3Icon className='h-6' />
+            <UserCircleIcon className='h-6' />
+          </div>
+        </div>
     </header>
   )
 }
